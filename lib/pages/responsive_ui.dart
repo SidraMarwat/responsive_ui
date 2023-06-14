01b7295d0979e284f794a3562a0b8ca4556d85d0
SidraMarwat <sidra.gul2002@gmail.com>
2023-06-13 21:43:55 -0700

    first commit

---
+++ lib/pages/responsive_ui.dart
@@ -0,0 +1,253 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class ResponsiveUI extends StatefulWidget {
+  const ResponsiveUI({super.key});
+
+  @override
+  State<ResponsiveUI> createState() => _ResponsiveUIState();
+}
+
+class _ResponsiveUIState extends State<ResponsiveUI> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: SingleChildScrollView(
+      child: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 21.w),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(height: 45.h),
+              Text(
+                "Sign Up to NUTECH",
+                style: TextStyle(
+                  fontSize: 16.sp,
+                  fontWeight: FontWeight.w600,
+                  fontFamily: "Inter",
+                ),
+              ),
+              SizedBox(height: 10.h),
+              Row(
+                children: [
+                  Text(
+                    "Already have an account? ",
+                    style: TextStyle(
+                      fontSize: 14.sp,
+                      fontWeight: FontWeight.w400,
+                      fontFamily: "Inter",
+                      color: const Color(0xff969AA8),
+                    ),
+                  ),
+                  Text(
+                    "Login",
+                    style: TextStyle(
+                      fontSize: 14.sp,
+                      fontWeight: FontWeight.w400,
+                      fontFamily: "Inter",
+                      color: const Color(0xff6F8FFF),
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(height: 24.h),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "Name",
+                    style: TextStyle(
+                      fontSize: 14.sp,
+                      fontWeight: FontWeight.w500,
+                      fontFamily: "Inter",
+                      color: const Color(0xff969AA8),
+                    ),
+                  ),
+                  SizedBox(height: 9.h),
+                  TextField(
+                    decoration: InputDecoration(
+                        prefixIcon: const Icon(Icons.person),
+                        contentPadding: EdgeInsets.all(18.h),
+                        fillColor: const Color.fromARGB(255, 255, 255, 255),
+                        filled: true,
+                        hintText: "Enter your Full Name",
+                        hintStyle: TextStyle(
+                          fontSize: 14.sp,
+                          fontWeight: FontWeight.w400,
+                          fontFamily: "Inter",
+                          color: const Color(0xffABB3BB),
+                        ),
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(100.sp),
+                            borderSide: const BorderSide(
+                              color: Color(0xffF5F6FA),
+                            )),
+                        enabledBorder: OutlineInputBorder(
+                            borderSide: BorderSide(
+                          color: const Color(0xff6DA9E4).withOpacity(0),
+                        )),
+                        focusedBorder: const OutlineInputBorder(
+                            borderSide: BorderSide(
+                          color: Color(0xff6DA9E4),
+                        ))),
+                  ),
+                ],
+              ),
+              SizedBox(height: 12.h),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "Email or Phone Number",
+                    style: TextStyle(
+                      fontSize: 14.sp,
+                      fontWeight: FontWeight.w500,
+                      fontFamily: "Inter",
+                      color: const Color(0xff969AA8),
+                    ),
+                  ),
+                  SizedBox(height: 9.h),
+                  TextField(
+                    decoration: InputDecoration(
+                        prefixIcon: const Icon(Icons.mail),
+                        contentPadding: EdgeInsets.all(18.h),
+                        fillColor: const Color.fromARGB(255, 255, 255, 255),
+                        filled: true,
+                        hintText: "Enter your email or phone",
+                        hintStyle: TextStyle(
+                          fontSize: 14.sp,
+                          fontWeight: FontWeight.w400,
+                          fontFamily: "Inter",
+                          color: const Color(0xffABB3BB),
+                        ),
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10.sp),
+                            borderSide: const BorderSide(
+                              color: Color(0xffF5F6FA),
+                            )),
+                        enabledBorder: OutlineInputBorder(
+                            borderSide: BorderSide(
+                          color: const Color(0xff6DA9E4).withOpacity(0),
+                        )),
+                        focusedBorder: const OutlineInputBorder(
+                            borderSide: BorderSide(
+                          color: Color(0xff6DA9E4),
+                        ))),
+                  ),
+                ],
+              ),
+              SizedBox(height: 12.h),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "Password",
+                    style: TextStyle(
+                      fontSize: 14.sp,
+                      fontWeight: FontWeight.w500,
+                      fontFamily: "Inter",
+                      color: const Color(0xff969AA8),
+                    ),
+                  ),
+                  SizedBox(height: 9.h),
+                  TextField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                        prefixIcon: const Icon(Icons.lock),
+                        suffixIcon: const Icon(Icons.visibility_off),
+                        contentPadding: EdgeInsets.all(18.h),
+                        fillColor: const Color.fromARGB(255, 255, 255, 255),
+                        filled: true,
+                        hintText: "Enter your password",
+                        hintStyle: TextStyle(
+                          fontSize: 14.sp,
+                          fontWeight: FontWeight.w400,
+                          fontFamily: "Inter",
+                          color: const Color(0xffABB3BB),
+                        ),
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10.sp),
+                            borderSide: const BorderSide(
+                              color: Color(0xffF5F6FA),
+                            )),
+                        enabledBorder: OutlineInputBorder(
+                            borderSide: BorderSide(
+                          color: const Color(0xff6DA9E4).withOpacity(0),
+                        )),
+                        focusedBorder: const OutlineInputBorder(
+                            borderSide: BorderSide(
+                          color: Color(0xff6DA9E4),
+                        ))),
+                  ),
+                ],
+              ),
+              SizedBox(height: 12.h),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "Confirm Password",
+                    style: TextStyle(
+                      fontSize: 14.sp,
+                      fontWeight: FontWeight.w500,
+                      fontFamily: "Inter",
+                      color: const Color(0xff969AA8),
+                    ),
+                  ),
+                  SizedBox(height: 9.h),
+                  TextField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                        prefixIcon: const Icon(Icons.lock),
+                        suffixIcon: const Icon(Icons.visibility_off),
+                        contentPadding: EdgeInsets.all(18.h),
+                        fillColor: const Color.fromARGB(255, 255, 255, 255),
+                        filled: true,
+                        hintText: "Confirm your password",
+                        hintStyle: TextStyle(
+                          fontSize: 14.sp,
+                          fontWeight: FontWeight.w400,
+                          fontFamily: "Inter",
+                          color: const Color(0xffABB3BB),
+                        ),
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10.sp),
+                            borderSide: const BorderSide(
+                              color: Color(0xffF5F6FA),
+                            )),
+                        enabledBorder: OutlineInputBorder(
+                            borderSide: BorderSide(
+                          color: const Color(0xff6DA9E4).withOpacity(0),
+                        )),
+                        focusedBorder: const OutlineInputBorder(
+                            borderSide: BorderSide(
+                          color: Color(0xff6DA9E4),
+                        ))),
+                  ),
+                ],
+              ),
+              SizedBox(height: 29.h),
+              Column(
+                children: [
+                  SizedBox(
+                    width: double.infinity,
+                    height:54.h,  
+                    child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                            backgroundColor: const Color(0xff6DA9E4),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10.sp),
+                            )),
+                        onPressed: () => true,
+                        child: const Text("Create Account")),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    ));
+  }
+}
